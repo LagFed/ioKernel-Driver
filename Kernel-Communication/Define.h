d844b8772f2a6ab9150cbf91f2b28d76645c6d6b
LagFed <151773164+LagFed@users.noreply.github.com>
2023-11-22 12:26:43 -0500

    Update Define.h

--- Kernel-Communication/Define.h
+++ Kernel-Communication/Define.h
@@ -1,1 +1,17 @@
+#ifndef _DEFINES_H
+#define _DEFINES_H
 
+#include <ntifs.h>
+
+constexpr ULONG PROCESS_ID_CODE = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x1337, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+typedef struct _PROCESS_ID_REQUEST
+{
+  ULONG process_id;
+  wchar_t process_name[260];
+  PROCESS_ID_REQUEST, * PROCESS_ID_REQUEST; // This may cause some issues, will fix if it causes any BSOD during snapshot collection
+}
+
+extern "C" // I know this is going to annoy some developers
+NTKERNELAPI
+// Will finish code for structure later.
